0bf54a8731ca3d04543e767552785bdb45cc99dd
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-16 07:46:56 +0200

    rename project to "location"

--- src/main/java/com/polovnev/country/LocationApplication.java
+++ src/main/java/com/polovnev/country/LocationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CountryApplication {
+public class LocationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CountryApplication.class, args);
+		SpringApplication.run(LocationApplication.class, args);
 	}
 
 }
